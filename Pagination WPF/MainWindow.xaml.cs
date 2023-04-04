628d341276bdb9213ef5e48e2e37ec3ed46ab590
trinhthelong <trinhthelong.bav@gmail.com>
2023-04-04 11:32:57 +0700

    Add project files.

---
+++ Pagination WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pagination_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sideBar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = sideBar.SelectedItem as NavButton;
+            navFrame.Navigate(selected?.Navlink);
+        }
+    }
+}
